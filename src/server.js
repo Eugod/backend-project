f20c92c6a5f9d99804a28257e2a9aacf69a5681e
Eugenio <eugenio28.04@gmail.com>
2025-11-07 19:01:03 -0300

    feat: server setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
-require('dotenv').config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
-const express = require('express');
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+})
 
 app.get("/", (req, res) => {
     res.send("Server is running");
